2c701c17ab40cdc8f34377e6b5eee1cc5bf15f86
TeraSurror <harshshelar22@gmail.com>
2025-12-12 21:29:13 -0500

    init.lua fix

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -61,13 +61,10 @@ lspconfig.gopls.setup({
   },
 })
 
-return {}
-
 -- OCAML
 lspconfig.ocamllsp.setup({
     cmd = { "ocamllsp" },
     filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" },
-    root_dir = lsp.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
-    on_attach = on_attach,
+    root_dir = lspconfig.util.root_pattern("*.opam", "esy.json", "package.json", ".git", "dune-project", "dune-workspace"),
     capabilities = capabilities
 })
